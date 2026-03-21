3eadcf8a202519a83402edc4c708d0c42703a458
stCarolas <stcarolas@gmail.com>
2026-03-21 23:53:32 +0300

    add command listener

---
+++ src/main/java/io/github/opendonationassistant/reel/listener/CommandListener.java
@@ -0,0 +1,30 @@
+package io.github.opendonationassistant.reel.listener;
+
+import io.github.opendonationassistant.events.MessageProcessor;
+import io.micronaut.messaging.annotation.MessageHeader;
+import io.micronaut.rabbitmq.annotation.Queue;
+import io.micronaut.rabbitmq.bind.RabbitAcknowledgement;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class CommandListener {
+
+  private MessageProcessor processor;
+
+  @Inject
+  public CommandListener(MessageProcessor processor) {
+    this.processor = processor;
+  }
+
+
+  @Queue(io.github.opendonationassistant.rabbit.Queue.Reel.COMMAND)
+  public void listen(
+    @MessageHeader String type,
+    byte[] payment,
+    RabbitAcknowledgement ack
+  ) {
+    processor.process(type, payment, ack);
+  }
+  
+}
